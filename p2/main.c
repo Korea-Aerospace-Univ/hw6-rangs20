4866fe0471d61f1d82df8ad38453b3d01421c0de
2025050066 <hrhr0107@gmail.com>
2026-05-27 16:49:57 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -7,15 +7,16 @@ int main() {
 	int num2[20];
 	int sum = 0;
 	for (int* p = num1; p < num1 + N; p++) {
-		scanf(" %d", p);
+		scanf("%d", p);
 	}
 	for (int* q = num2; q < num2 + N; q++) {
-		scanf(" %d", q);
+		scanf("%d", q);
 	}
 	int* p = num1;
-	int* q = num2;
-	for (int i = 0; i < N; i++) {
-		sum = *(p + i) + *(q + N - 1 - i);
+	int* q = (num2 + (N-1)); //num2 맨끝 주소
+	for (int* p = num1; p < num1 + N; p++) {
+        sum = *p + *q; //p의 위치 + q의 위치
+		q--;// q를 num2의 맨끝주소부터 시작하기 떄문에 포인트 변수를 1씩 뺴서 주소에 -1을 하며 역순으로 가도록
 		printf(" %d", sum);
 	}
 }
